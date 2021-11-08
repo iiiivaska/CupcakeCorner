ec6df5b724bb86ebb7a9a5b9c2f05fd2dca3fbc8
iiiivaska <iiiivaska@yandex.ru>
2021-11-08 20:37:15 +0300

    first screen complete

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,26 +7,66 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var username = ""
-    @State private var email = ""
-    var disableForm: Bool {
-        username.count < 5 || email.count < 5
-    }
-    var body: some View {
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
             }
-            
-            Section {
-                Button("Create account") {
-                    print("Creating account...")
+        }
+    }
+    
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
+
+struct ContentView: View {
+    @ObservedObject var order = Order()
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(0..<Order.types.count) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+                
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                        Text("Any special requests?")
+                    }
+                    
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting) {
+                            Text("Add extra frosting")
+                        }
+                        
+                        Toggle(isOn: $order.addSprinkles) {
+                            Text("Add extra sprinkles")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: AddressView(order: order)) {
+                        Text("Delivery details")
+                    }
                 }
             }
-            //.disabled(username.isEmpty || email.isEmpty)
-            .disabled(disableForm)
+            .navigationTitle("Cupcake Corner")
         }
     }
 }
