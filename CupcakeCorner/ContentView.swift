11d7fc31eb73de7c530af0fafe902783b4b384c7
iiiivaska <iiiivaska@yandex.ru>
2021-11-08 20:58:37 +0300

    AdressView completed

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -24,6 +24,19 @@ class Order: ObservableObject {
     
     @Published var extraFrosting = false
     @Published var addSprinkles = false
+    
+    @Published var name = ""
+    @Published var streetAddress = ""
+    @Published var city = ""
+    @Published var zip = ""
+    
+    var hasValidAdress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        
+        return true
+    }
 }
 
 struct ContentView: View {
